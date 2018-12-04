40d358099f290b63f5aad3524e8c2070c791bbad
ngodavidhuy <ngodavidhuy@gmail.com>
2018-12-03 17:31:21 -0800

    Completed Tests Up To readOne

--- datastore/counter.js
+++ datastore/counter.js
@@ -38,9 +38,10 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+  readCounter((err, data) => {
+    writeCounter((data + 1), callback);
+  });
 };
 
 
--- datastore/index.js
+++ datastore/index.js
@@ -8,26 +8,35 @@ var items = {};
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  counter.getNextUniqueId((err, id) => {
+    var fileLocation = path.join(exports.dataDir, `${id}.txt`);
+    fs.writeFile(fileLocation, text, (err) => {
+      if (err) {
+        throw ('error writing text');
+      } else {
+        callback(null, { id, text });
+      }
+    });
+  });
 };
 
 exports.readAll = (callback) => {
-  var data = [];
-  _.each(items, (text, id) => {
-    data.push({ id, text });
+  fs.readdir(exports.dataDir, (err, items) => {
+    var arr = [];
+    items.forEach(item => {
+      item = item.slice(0, 5);
+      arr.push({ id: item, text: item });
+    });
+    callback(null, arr);
   });
-  callback(null, data);
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  let fileLocation = path.join(exports.dataDir, `${id}.txt`);
+  fs.readFile(fileLocation, 'utf8', (err, text) => {
+    var input = { id, text };
+    callback(err, input);
+  });
 };
 
 exports.update = (id, text, callback) => {
