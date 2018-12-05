47b5554a3f438ceb81dc8449fc5e2ef4cd1ec618
ngodavidhuy <ngodavidhuy@gmail.com>
2018-12-04 16:54:30 -0800

    Passed All Minimum Requirements

--- datastore/index.js
+++ datastore/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
+const Promise = require('bluebird');
 
 var items = {};
 
@@ -20,17 +21,31 @@ exports.create = (text, callback) => {
   });
 };
 
+
+var readFilePromise = Promise.promisify(fs.readFile);
+var readDirPromise = Promise.promisify(fs.readdir);
+
 exports.readAll = (callback) => {
-  fs.readdir(exports.dataDir, (err, items) => {
-    var arr = [];
-    items.forEach(item => {
-      item = item.slice(0, 5);
-      arr.push({ id: item, text: item });
-    });
-    callback(null, arr);
+  let data = readDirPromise(exports.dataDir).then(files => {
+    return files.map(id => {
+      let fileLocation = path.join(exports.dataDir, id);
+      return readFilePromise(fileLocation, 'utf8').then(text => {
+        return { id: path.basename(id, '.txt'), text }
+      });
+    })
   });
+
+  Promise.all(data).then((data) => { callback(null, data); })
 };
 
+// fs.readdir(exports.dataDir, (err, items) => {
+//   var arr = [];
+//   items.forEach(item => {
+//     item = item.slice(0, 5);
+//     arr.push({ id: item, text: item });
+//   });
+//   callback(null, arr);
+// });
 exports.readOne = (id, callback) => {
   let fileLocation = path.join(exports.dataDir, `${id}.txt`);
   fs.readFile(fileLocation, 'utf8', (err, text) => {
